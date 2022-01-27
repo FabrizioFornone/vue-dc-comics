d7a66dad3ef5fd0f844814b812b8d1a93779416d
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-27 18:31:54 +0100

    SecondFooterBox Done

--- src/components/SecondFooterBox.vue
+++ src/components/SecondFooterBox.vue
@@ -1,7 +1,26 @@
 <template>
   <footer>
-    <div class="container"></div>
-    <button><a href="#">SIGN-UP NOW!</a></button>
+    <div class="container">
+      <div class="d-flex justify-content-between">
+        <button><a href="#">SIGN-UP NOW!</a></button>
+        <div class="d-flex align-items-center">
+          <h3><a href="#">FOLLOW US</a></h3>
+          <a href="#"><img src="@/assets/img/footer-facebook.png" alt="" /></a>
+          <a href="#">
+            <img src="@/assets/img/footer-twitter.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="@/assets/img/footer-youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="@/assets/img/footer-pinterest.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="@/assets/img/footer-periscope.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
