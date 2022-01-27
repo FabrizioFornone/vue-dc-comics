c6ef8eab3ec90410124d59bcb952e7ebb178de98
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-27 18:35:54 +0100

    FooterBox fixed structure

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -5,44 +5,44 @@
         <div>
           <h3>DC Comics</h3>
           <ul>
-            <li><a href="#"></a>Characters</li>
-            <li><a href="#"></a>Comics</li>
-            <li><a href="#"></a>Movies</li>
-            <li><a href="#"></a>TV</li>
-            <li><a href="#"></a>Games</li>
-            <li><a href="#"></a>Videos</li>
-            <li><a href="#"></a>News</li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
           </ul>
           <h3>SHOP</h3>
           <ul>
-            <li><a href="#"></a>Shop DC</li>
-            <li><a href="#"></a>Shop DC Collectibles</li>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
           </ul>
         </div>
         <div>
           <h3>DC</h3>
           <ul>
-            <li><a href="#"></a>Terms Of Use</li>
-            <li><a href="#"></a>Privacy policy (New)</li>
-            <li><a href="#"></a>Ad Choice</li>
-            <li><a href="#"></a>Advertising</li>
-            <li><a href="#"></a>Jobs</li>
-            <li><a href="#"></a>Subscriptions</li>
-            <li><a href="#"></a>Talent Workshops</li>
-            <li><a href="#"></a>CPSC Certificates</li>
-            <li><a href="#"></a>Ratings</li>
-            <li><a href="#"></a>Shop Help</li>
-            <li><a href="#"></a>Contact Us</li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choice</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
         <div>
           <h3>SITES</h3>
           <ul>
-            <li><a href="#"></a>DC</li>
-            <li><a href="#"></a>MAD Magazine</li>
-            <li><a href="#"></a>DC Kids</li>
-            <li><a href="#"></a>Dc Universe</li>
-            <li><a href="#"></a>DC Power Visa</li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">Dc Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
           </ul>
         </div>
       </div>
