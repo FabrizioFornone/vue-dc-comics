99eec7a87c6e57d01e5072b60f2c1598493c188d
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-27 15:38:39 +0100

    Header Done

---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div>
+      <img src="@/assets/img/dc-logo.png" alt="" />
+    </div>
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/headerStyle.scss";
+</style>
